a91885e8ec16966348090232228896e1b43d08c6
David Vargas <dvargas92495@gmail.com>
2022-10-27 18:45:12 -0400

    Publish oura ring to roam depot

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-# oura-ring
+# Oura Ring
         
-Description for oura-ring.
+Import your Oura Ring daily summaries on a given day into your daily note page!
+
+## Usage
   
-For full documentation, checkout https://roamjs.com/extensions/oura-ring!
+You'll first need to add your personal access token associated with your Oura Ring account to the `Token` field in your Roam Depot Settings. The extension needs this in order to access your personal data. [Click here](https://cloud.ouraring.com/personal-access-tokens), to generate your own personal access token.
+
+To import your Oura Ring data to your daily note page, open the Command Palette and enter "Import Oura Ring". If the current page is a Daily note page, it will query the day before the page title, since you usually want to track last night's sleep. Otherwise, it will query yesterday's data by default. It will output the following text:
+
+```
+Bedtime Start:: hh:mm:ss
+Bedtime End:: hh:mm:ss
+Sleep Duration:: hh:mm:ss
+Total Sleep:: hh:mm:ss
+Total Awake:: hh:mm:ss
+Light Sleep:: hh:mm:ss
+Rem Sleep:: hh:mm:ss
+Deep Sleep:: hh:mm:ss
+Day Start:: hh:mm:ss
+Day End:: hh:mm:ss
+Low Activity:: hh:mm:ss
+Medium Activity:: hh:mm:ss
+High Activity:: hh:mm:ss
+Rest Activity:: hh:mm:ss
+Readiness Score:: hh:mm:ss
+```
+
+You can also import the data by creating a button by typing `{{import oura ring}}` into a page and clicking the button. 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,251 @@
 import runExtension from "roamjs-components/util/runExtension";
+import getChildrenLengthByParentUid from "roamjs-components/queries/getChildrenLengthByParentUid";
+import format from "date-fns/format";
+import subDays from "date-fns/subDays";
+import getPageTitleByBlockUid from "roamjs-components/queries/getPageTitleByBlockUid";
+import getParentUidByBlockUid from "roamjs-components/queries/getParentUidByBlockUid";
+import getPageUidByPageTitle from "roamjs-components/queries/getPageUidByPageTitle";
+import getFullTreeByParentUid from "roamjs-components/queries/getFullTreeByParentUid";
+import updateBlock from "roamjs-components/writes/updateBlock";
+import createBlock from "roamjs-components/writes/createBlock";
+import getOrderByBlockUid from "roamjs-components/queries/getOrderByBlockUid";
+import createButtonObserver from "roamjs-components/dom/createButtonObserver";
+import getUidsFromButton from "roamjs-components/dom/getUidsFromButton";
+import apiGet from "roamjs-components/util/apiGet";
 
 export default runExtension({
   run: (args) => {
     args.extensionAPI.settings.panel.create({
       tabTitle: "oura-ring",
-      settings: [],
+      settings: [
+        {
+          id: "token",
+          name: "Token",
+          description:
+            "Your Oura Ring personal access token, accessible from https://cloud.ouraring.com/personal-access-tokens.",
+          action: { type: "input", placeholder: "xxxxxx" },
+        },
+      ],
     });
+
+    const OURA_COMMAND = "Import Oura Ring";
+
+    const secondsToTimeString = (s: number) => {
+      const hours = `${Math.floor(s / 3600)}`;
+      const minutes = `${Math.floor(s / 60) % 60}`;
+      const seconds = `${s % 60}`;
+      return `${hours.padStart(2, "0")}:${minutes.padStart(
+        2,
+        "0"
+      )}:${seconds.padStart(2, "0")}`;
+    };
+
+    const importOuraRing = async (blockUid: string) => {
+      const parentUid = getParentUidByBlockUid(blockUid);
+      const pageTitle = getPageTitleByBlockUid(blockUid);
+      const dateFromPage = window.roamAlphaAPI.util.pageTitleToDate(pageTitle);
+      const token = args.extensionAPI.settings.get("token");
+      if (!token) {
+        window.roamAlphaAPI.updateBlock({
+          block: {
+            string: `Error: Could not find the required "Token" attribute configured in the [[roam/js/oura-ring]] page.`,
+            uid: blockUid,
+          },
+        });
+        return;
+      }
+      const dateToUse = isNaN(dateFromPage.valueOf())
+        ? new Date()
+        : dateFromPage;
+      const formattedDate = format(subDays(dateToUse, 1), "yyyy-MM-dd");
+      const bullets: string[] = [];
+      return Promise.all([
+        apiGet<{
+          sleep: [
+            {
+              score: number;
+              efficiency: number;
+              duration: number;
+              bedtime_start: string;
+              bedtime_end: string;
+              total: number;
+              awake: number;
+              onset_latency: number;
+              light: number;
+              rem: number;
+              deep: number;
+              hr_lowest: number;
+              hr_average: number;
+              rmssd: number;
+            }
+          ];
+        }>({
+          domain: `https://api.ouraring.com/v1`,
+          path: "sleep",
+          anonymous: true,
+          data: {
+            start: formattedDate,
+            end: formattedDate,
+            access_token: token,
+          },
+        }),
+        apiGet<{
+          activity: [
+            {
+              day_start: number;
+              day_end: number;
+              score: number;
+              low: number;
+              medium: number;
+              high: number;
+              rest: number;
+              steps: number;
+            }
+          ];
+        }>({
+          domain: `https://api.ouraring.com/v1`,
+          path: "activity",
+          anonymous: true,
+          data: {
+            start: formattedDate,
+            end: formattedDate,
+            access_token: token,
+          },
+        }),
+        apiGet<{
+          readiness: [
+            {
+              score: number;
+            }
+          ];
+        }>({
+          domain: `https://api.ouraring.com/v1`,
+          path: "readiness",
+          anonymous: true,
+          data: {
+            start: formattedDate,
+            end: formattedDate,
+            access_token: token,
+          },
+        }),
+      ])
+        .then(([sleepData, activityData, readinessData]) => {
+          const sleep = sleepData.sleep[0];
+          if (!sleep) {
+            bullets.push(
+              `There is no sleep data available for ${formattedDate}`
+            );
+          } else {
+            const { bedtime_start, bedtime_end } = sleep;
+            const formattedStart = format(new Date(bedtime_start), "hh:mm:ss");
+            const formattedEnd = format(new Date(bedtime_end), "hh:mm:ss");
+            bullets.push(
+              `Bedtime Start:: ${formattedStart}`,
+              `Bedtime End:: ${formattedEnd}`,
+              `Sleep Score:: ${sleep.score}`,
+              `Sleep Efficiency:: ${sleep.efficiency}`,
+              `Sleep Duration:: ${secondsToTimeString(sleep.duration)}`,
+              `Total Sleep:: ${secondsToTimeString(sleep.total)}`,
+              `Total Awake:: ${secondsToTimeString(sleep.awake)}`,
+              `Sleep Latency:: ${secondsToTimeString(sleep.onset_latency)}`,
+              `Light Sleep:: ${secondsToTimeString(sleep.light)}`,
+              `Rem Sleep:: ${secondsToTimeString(sleep.rem)}`,
+              `Deep Sleep:: ${secondsToTimeString(sleep.deep)}`,
+              `Resting Heart Rate:: ${sleep.hr_lowest}`,
+              `Average Heart Rate:: ${sleep.hr_average}`,
+              `Heart Rate Variability:: ${sleep.rmssd}`
+            );
+          }
+
+          const activity = activityData.activity[0];
+          if (!activity) {
+            bullets.push(
+              `There is no activity data available for ${formattedDate}`
+            );
+          } else {
+            const { day_start, day_end } = activity;
+            const formattedStart = format(new Date(day_start), "hh:mm:ss");
+            const formattedEnd = format(new Date(day_end), "hh:mm:ss");
+            bullets.push(
+              `Day Start:: ${formattedStart}`,
+              `Day End:: ${formattedEnd}`,
+              `Activity Score:: ${activity.score}`,
+              `Low Activity:: ${secondsToTimeString(activity.low * 60)}`,
+              `Medium Activity:: ${secondsToTimeString(activity.medium * 60)}`,
+              `High Activity:: ${secondsToTimeString(activity.high * 60)}`,
+              `Rest Activity:: ${secondsToTimeString(activity.rest * 60)}`,
+              `Steps:: ${activity.steps}`
+            );
+          }
+
+          const readiness = readinessData.readiness[0];
+          if (!readiness) {
+            bullets.push(
+              `There is no activity data available for ${formattedDate}`
+            );
+          } else {
+            bullets.push(`Readiness Score:: ${readiness.score}`);
+          }
+
+          const base = getOrderByBlockUid(blockUid);
+          return Promise.all([
+            updateBlock({ uid: blockUid, text: bullets[0] }),
+            ...bullets
+              .slice(1)
+              .map((text, order) =>
+                createBlock({ node: { text }, parentUid, order: order + base })
+              ),
+          ]);
+        })
+        .catch((e) => {
+          if (e.response?.status === 401) {
+            return window.roamAlphaAPI.updateBlock({
+              block: {
+                string: `The token used (${token}) is not authorized to access oura ring.`,
+                uid: blockUid,
+              },
+            });
+          }
+          window.roamAlphaAPI.updateBlock({
+            block: {
+              string:
+                "Unexpected Error thrown. Email support@roamjs.com for help!",
+              uid: blockUid,
+            },
+          });
+        });
+    };
+
+    createButtonObserver({
+      attribute: OURA_COMMAND.replace(/\s/g, "-"),
+      shortcut: OURA_COMMAND,
+      render: (b) =>
+        (b.onclick = (e) => {
+          importOuraRing(getUidsFromButton(b).blockUid);
+          e.preventDefault();
+          e.stopPropagation();
+        }),
+    });
+
+    window.roamAlphaAPI.ui.commandPalette.addCommand({
+      label: "Import Oura Ring Data",
+      callback: async () => {
+        const parentUid = await window.roamAlphaAPI.ui.mainWindow
+          .getOpenPageOrBlockUid()
+          .then(
+            (uid) => uid || window.roamAlphaAPI.util.dateToPageUid(new Date())
+          );
+        const blockUid = await createBlock({
+          parentUid,
+          order: getChildrenLengthByParentUid(parentUid),
+          node: { text: "" },
+        });
+        importOuraRing(blockUid);
+      },
+    });
+
+    return {
+      commands: ["Import Oura Ring Data"],
+    };
   },
 });
